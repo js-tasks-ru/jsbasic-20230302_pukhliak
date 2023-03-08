310d1febbaa7e50894f403bd55003f5c62f1952c
Pukhliak <pukhliak@gmail.com>
2023-03-08 16:51:31 +0300

    Extend the username validation function

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,15 +10,25 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  let str = name;
+
+  if (str === null) {
+    return false;
+  } else if (str.length == 0 || str.length < 4) {
+    return false;
+  } else if (str.indexOf(" ") !== -1) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
