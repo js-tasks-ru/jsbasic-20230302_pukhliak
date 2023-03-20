d2e1e720dab799d8312a3ea2068dca4651c82835
Pukhliak <pukhliak@gmail.com>
2023-03-20 22:30:27 +0300

    complite 5-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = ``;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age <= age) {
+      str += `${users[i].name}, ${users[i].balance}\n`;
+    }
+  }
+  let newStr = str.split("\n");
+  newStr.pop();
+  return newStr.join(`\n`);
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ");
+  let newArr = arr
+    .map((i) => Number(i))
+    .filter((i) => !isNaN(i))
+    .sort((a, b) => a - b);
+  let obj = {};
+  obj.min = newArr[0];
+  obj.max = newArr[newArr.length - 1];
+  return obj;
 }
