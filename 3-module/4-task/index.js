b8e9ce945d5c0b3f68f7905790062babacd51df5
Pukhliak <pukhliak@gmail.com>
2023-03-29 16:25:09 +0300

    replaced the code with three functions

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,11 +1,6 @@
 function showSalary(users, age) {
-  let str = ``;
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].age <= age) {
-      str += `${users[i].name}, ${users[i].balance}\n`;
-    }
-  }
-  let newStr = str.split("\n");
-  newStr.pop();
-  return newStr.join(`\n`);
+  return users
+    .filter((users) => users.age <= age)
+    .map((users) => `${users.name}, ${users.balance}`)
+    .join("\n");
 }
