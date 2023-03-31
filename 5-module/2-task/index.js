7cc402cece194955eaad99d01c6c69e619aba5b3
Pukhliak <pukhliak@gmail.com>
2023-03-31 13:06:13 +0300

    5-module complite

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+  let isHidden = false;
+  button.addEventListener("click", () => {
+    isHidden = !isHidden;
+    text.hidden = isHidden;
+  });
 }
